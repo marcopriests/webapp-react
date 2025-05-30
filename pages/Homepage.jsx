9a4637cd224fa5b50b30644f11d205bfd5bee510
marcopriests <prtmrc98@gmail.com>
2025-05-30 16:08:04 +0200

    [UPD] single card done

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const Homepage = () => {
     return (
-        <div>
-            homepage
-        </div>
+        <>
+            <div className="p-5">
+                <h1><b>Latest movies</b></h1>
+                <div className='container mt-4'>
+                    <div className="row">
+                        <div className="col-4 col-md-3 col-lg-2">
+                            <div className="card rounded-0 border-0">
+                                <img src="https://picsum.photos/200/300" alt="" className="card-img-top rounded-0 shadow-sm" />
+                                <div className='mt-2'>
+                                    <div className="fs-6"><b>Titolo film</b></div>
+                                    <div className="">Voto</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router'
 
 const Header = () => {
     return (
-        <div className='bg-dark p-1 d-flex align-items-center'>
-            <div className='col-1 p-3 me-2'>
+        <div className='bg-dark p-3 d-flex align-items-center'>
+            <div className='logo me-2'>
                 <img className='img-fluid' src="public/duck.png" alt="logo" />
             </div>
 
