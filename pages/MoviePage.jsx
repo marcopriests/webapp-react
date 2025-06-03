6fd226afb4c4b97ba7b7094599e3d1cb44825b5d
marcopriests <prtmrc98@gmail.com>
2025-06-03 16:07:44 +0200

    [UPD]

--- pages/MoviePage.jsx
+++ pages/MoviePage.jsx
@@ -1,74 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieCard from '../src/components/MovieCard'
+import axios from 'axios'
 
-const movies = [
-    {
-        id: 1,
-        title: 'Chi ha incastrato Roger Rabbit',
-        director: 'Robert Zemeckis',
-        releaseDate: 1988,
-        averageVote: 9.9,
-        genre: ['animation, comedy']
-    },
-    {
-        id: 2,
-        title: 'Big Fish',
-        director: 'Tim Burton',
-        releaseDate: 2004,
-        averageVote: 7.8,
-        genre: ['romance, adventure, action, comedy, drama']
-    },
-    {
-        id: 3,
-        title: 'Il postino',
-        director: 'Massimo Troisi',
-        releaseDate: 1994,
-        averageVote: 6.5,
-        genre: ['comedy, drama, romance']
-    },
-    {
-        id: 4,
-        title: 'Le avventure acquatiche di Steve Zissou',
-        director: 'Wes Anderson',
-        releaseDate: 2005,
-        averageVote: 2.1,
-        genre: ['comedy, drama, adventure']
-    },
-    {
-        id: 5,
-        title: 'Chi ha incastrato Roger Rabbit',
-        director: 'Robert Zemeckis',
-        releaseDate: 1988,
-        averageVote: 9.9,
-        genre: ['animation, comedy']
-    },
-    {
-        id: 6,
-        title: 'Big Fish',
-        director: 'Tim Burton',
-        releaseDate: 2004,
-        averageVote: 7.8,
-        genre: ['romance, adventure, action, comedy, drama']
-    },
-    {
-        id: 7,
-        title: 'Il postino',
-        director: 'Massimo Troisi',
-        releaseDate: 1994,
-        averageVote: 6.5,
-        genre: ['comedy, drama, romance']
-    },
-    {
-        id: 8,
-        title: 'Le avventure acquatiche di Steve Zissou',
-        director: 'Wes Anderson',
-        releaseDate: 2005,
-        averageVote: 2.1,
-        genre: ['comedy, drama, adventure']
-    }
-]
+// const initialMovies = [
+//     {
+//         id: 1,
+//         title: 'Chi ha incastrato Roger Rabbit',
+//         director: 'Robert Zemeckis',
+//         releaseDate: 1988,
+//         averageVote: 9.9,
+//         genre: ['animation, comedy']
+//     },
+//     {
+//         id: 2,
+//         title: 'Big Fish',
+//         director: 'Tim Burton',
+//         releaseDate: 2004,
+//         averageVote: 7.8,
+//         genre: ['romance, adventure, action, comedy, drama']
+//     },
+//     {
+//         id: 3,
+//         title: 'Il postino',
+//         director: 'Massimo Troisi',
+//         releaseDate: 1994,
+//         averageVote: 6.5,
+//         genre: ['comedy, drama, romance']
+//     },
+//     {
+//         id: 4,
+//         title: 'Le avventure acquatiche di Steve Zissou',
+//         director: 'Wes Anderson',
+//         releaseDate: 2005,
+//         averageVote: 2.1,
+//         genre: ['comedy, drama, adventure']
+//     },
+//     {
+//         id: 5,
+//         title: 'Chi ha incastrato Roger Rabbit',
+//         director: 'Robert Zemeckis',
+//         releaseDate: 1988,
+//         averageVote: 9.9,
+//         genre: ['animation, comedy']
+//     },
+//     {
+//         id: 6,
+//         title: 'Big Fish',
+//         director: 'Tim Burton',
+//         releaseDate: 2004,
+//         averageVote: 7.8,
+//         genre: ['romance, adventure, action, comedy, drama']
+//     },
+//     {
+//         id: 7,
+//         title: 'Il postino',
+//         director: 'Massimo Troisi',
+//         releaseDate: 1994,
+//         averageVote: 6.5,
+//         genre: ['comedy, drama, romance']
+//     },
+//     {
+//         id: 8,
+//         title: 'Le avventure acquatiche di Steve Zissou',
+//         director: 'Wes Anderson',
+//         releaseDate: 2005,
+//         averageVote: 2.1,
+//         genre: ['comedy, drama, adventure']
+//     }
+// ]
 
 const Homepage = () => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:3000/movies').then(res => {
+            console.log(res.data)
+            setMovies(res.data)
+        })
+    }, [])
+
     return (
         <>
             <div className="p-5">
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const MovieCard = ({ movie }) => {
-    const { title, averageVote, id } = movie
+    const { title, image, averageVote, id } = movie
 
     const stars = []
 
@@ -22,7 +22,7 @@ const MovieCard = ({ movie }) => {
         <div className="col-4 col-md-3 col-lg-2">
             <Link className='text-decoration-none' to={`/${id}`}>
                 <div className="card rounded-0 border-0 mb-4">
-                    <img src="https://picsum.photos/200/300" alt="" className="card-img-top rounded-0 shadow-sm" />
+                    <img src={image} alt="" className="card-img-top rounded-0 shadow-sm" />
                     <div className='mt-2'>
                         <div className="fs-6"><b>{title}</b></div>
                         <div className="">{stars}</div>
